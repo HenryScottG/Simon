79d0cfc29b81a930ee81d6c94152cd43d68b1418
Scott Gilson <gilsonscott@johndeere.com>
2020-07-11 13:00:54 -0500

    update listener player

--- script.js
+++ script.js
@@ -3,12 +3,63 @@ console.log('ok');
 let start=document.querySelector('#start');
 start.addEventListener('click',(event)=>{
 console.log('start');
+let cells = document.getElementsByClassName('cell');
+let compSeq=[];
+// computer picks the sequence of colors
+for (let i = 0; i < 4; i++){
+    compSeq.push(Math.floor(Math.random()*4 +1));
+    console.log(compSeq[i]);
+}
+let userTurn=0;
+function input(turn,color){
+let playerSeq=-1;
+    // for (let i = 0; i < 4; i++){
+        // compSeq.push(Math.floor(Math.random()*4 +1));
+        if (color==='cell0'){
+            playerSeq=1;
+        } else if (color==='cell1'){
+            playerSeq=2;
+        } else if (color==='cell2'){
+            playerSeq=3;
+        } else if (color==='cell3'){
+            playerSeq=4;
+        } 
+        if (compSeq[turn]===playerSeq){ //matching
+            console.log('winner');
+        }  else{
+            console.log('loser');
+        }
+
+        console.log(compSeq[turn]);
+        console.log(color);
+}
+// creating the event listeners for the cells
+for(let i = 0; i < cells.length; i++){
+    cells[i].addEventListener('click', function(){
+        let cellId = this.getAttribute('id'); // this circle
+        cells[i].fadeTo('fast', 0.5).fadeTo('fast', 1);
+        // userInput.push(parseInt(circleId.charAt(circleId.length - 1))); // grabbing last user input's circle array
+        // roundStart(); // running program again
+        input(userTurn,cellId);
+        userTurn++; // increment user turn by 1
+        });
+    }
 });
+
+
+
+
+
 // add listener for End button
 let end=document.querySelector('#end');
 end.addEventListener('click',(event)=>{
 console.log('end');
 });
-// start a game
-var score=0;
 
+
+
+
+// let myCells=document.querySelectorAll('.cell');
+// myCells.forEach(cell=>{
+//     document.addEventListener('click', getCell);  
+// })
